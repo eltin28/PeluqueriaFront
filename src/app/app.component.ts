bca28a0a2b5c4ddffc7ab9367494e98e6e132f26
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-10-22 15:52:52 -0500

    Inicio del repositorio frontend para software II proyecto peluqueria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'peluqueria';
+  footer = 'Universidad del Quindio - 2024-2'
 }
